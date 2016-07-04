f6df1594d43f100076126565eb5b12ae9a9a4865
Pliable Pixels <pliablepixels@gmail.com>
2016-07-04 04:06:59 -0400

    updates

--- server/src/examples/nvrcli/nvrcli.cpp
+++ server/src/examples/nvrcli/nvrcli.cpp
@@ -14,7 +14,39 @@ MotionDetector* motion[MAX_CAMS];
 Listener listener;
 HttpController* httpController;
 Application app;
+int cam_ndx=-1;
 
+
+// adds a new camera and motion detector
+void cmd_add()
+{
+	string name;
+	string source;
+	cout << "camera name:";
+	cin >> name;
+	//cout << "RTSP source:";
+	//cin >> source;
+	// fixme- hardcoded
+	cam[0] = new NetworkAVInput ( name, "rtsp://170.93.143.139:1935/rtplive/0b01b57900060075004d823633235daa" );
+	cam_ndx++;
+    	cout << "Adding:"<<cam[cam_ndx]->name() << endl;
+    	cout << cam[cam_ndx]->source() << endl;
+    	app.addThread( cam[cam_ndx] );
+ 	cam[cam_ndx]->start();
+
+	// motion detect for cam
+	motion[cam_ndx] = new MotionDetector( "modect-"+name );
+	motion[cam_ndx]->registerProvider(*cam[cam_ndx] );
+	app.addThread( motion[cam_ndx] );
+	motion[cam_ndx]->start();
+
+	// stop the listener, add new camera, restart
+	listener.stop();
+	//listener.removeController(httpController);
+  	httpController->addStream("watch-"+name,*cam[cam_ndx]);
+	//listener.addController(httpController);
+	listener.start();
+}
 
 void cmd_help()
 {
@@ -29,9 +61,9 @@ void cmd_unknown()
 
 void cli(Application app)
 {
-	for (int i=0; i<MAX_CAMS; i++) { cam[i] = NULL; motion[i] = NULL; }
 	unordered_map<std::string, std::function<void()>> cmd_map;
 	cmd_map["help"] = &cmd_help;
+	cmd_map["add"] = &cmd_add;
 	
 	
 	string command;
@@ -53,7 +85,7 @@ void cli(Application app)
 int main( int argc, const char *argv[] )
 {
     debugInitialise( "starter-example", "", 0 );
-	cout << " \n---------------------- NVRCLI ------------------\n"
+    cout << " \n---------------------- NVRCLI ------------------\n"
 		     " Type help to get started\n"
 		     " ------------------------------------------------\n\n";
 
@@ -61,28 +93,16 @@ int main( int argc, const char *argv[] )
 
     avInit();
 
-
-   // Two RTSP sources 
-    cam[0] = new NetworkAVInput ( "cam1", "rtsp://170.93.143.139:1935/rtplive/0b01b57900060075004d823633235daa" );
-	cout << cam[0]->name() << endl;
-	cout << cam[0]->source() << endl;
-    app.addThread( cam[0] );
-
-	// motion detect for cam1
-	motion[0] = new MotionDetector( "modectcam1" );
-  	motion[0]->registerProvider(*cam[0] );
-   	app.addThread( motion[0] );
-
-
-    	app.addThread( &listener );
-
-    httpController = new HttpController( "watch", 9292 );
-    httpController->addStream("watchcam1",*cam[0]);
+    //fixme: convert this to a list later
+    for (int i=0; i<MAX_CAMS; i++) { cam[i] = NULL; motion[i] = NULL; }
+    
+  httpController = new HttpController( "watch", 9292 );
 
 	
-    	listener.addController( httpController );
+  listener.addController( httpController );
+  app.addThread( &listener );
 
-	thread t1(cli,app);
-	app.run();
-   	cout << "Never here";
+  thread t1(cli,app);
+  app.run();
+  cout << "Never here";
 }
