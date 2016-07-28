c993e7d31b493ecbbe94b58e6ad3c93324afe192
Philip Coombes <phil@web2wire.com>
2016-07-28 07:32:31 +0000

    Tweaked. Now writes movie files as well.

--- server/src/examples/example10.cpp
+++ server/src/examples/example10.cpp
@@ -21,15 +21,25 @@ int main( int argc, const char *argv[] )
     Application app;
 
     NetworkAVInput input( "input", "http://kxhcm10/nphMotionJpeg?Resolution=640x480&Quality=Standard", "mjpeg" );
+    //NetworkAVInput input( "input", "Shop Door Camera.mp4" );
+    //NetworkAVInput input( "input", "mcem0_head.mpg" );
     app.addThread( &input );
 
     FaceDetector detector( "detector" );
     detector.registerProvider( input );
     app.addThread( &detector );
 
-    LocalFileOutput output( detector.cname(), "/transfer" );
-    output.registerProvider( detector );
-    app.addThread( &output );
+    // Writes out individual images
+    //LocalFileOutput output( detector.cname(), "/transfer" );
+    //output.registerProvider( detector );
+    //app.addThread( &output );
+
+    // Writes out movie
+    VideoParms videoParms( 320, 240 );
+    AudioParms audioParms;
+    MovieFileOutput output2( detector.cname(), "/transfer", "mp4", 60, videoParms, audioParms );
+    output2.registerProvider( detector );
+    app.addThread( &output2 );
 
     app.run();
 }
