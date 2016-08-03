6dd158684d48983f64557d3403568743bbf0baab
Philip Coombes <phil@web2wire.com>
2016-08-03 08:41:27 +0100

    Added dependent libraries for dlib and also tweaks for Video4Linux support.

--- tools/docker/ozonedev/bootstrap-ozone.sh
+++ tools/docker/ozonedev/bootstrap-ozone.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd $HOME
-mkdir $INSTALLDIR
-git clone https://github.com/ozonesecurity/ozonebase.git
+mkdir -p $INSTALLDIR
+if [ ! -d "ozonebase" ]
+then
+  git clone https://github.com/ozonesecurity/ozonebase.git
+fi
 cd $HOME/ozonebase
 git submodule update --init --recursive
 ( cd externals/openh264/ && make PREFIX="$INSTALLDIR" install )
-( cd externals/ffmpeg && PKG_CONFIG_PATH=$INSTALLDIR/lib/pkgconfig ./configure --enable-shared --enable-libopenh264 --prefix=$INSTALLDIR && make install )
+( cd externals/ffmpeg && PKG_CONFIG_PATH=$INSTALLDIR/lib/pkgconfig ./configure --enable-shared --enable-libv4l2 --enable-libopenh264 --prefix=$INSTALLDIR && make install )
+( cd externals/dlib && mkdir -p build && cd build && cmake .. -DCMAKE_PREFIX_PATH=$INSTALLDIR -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DCMAKE_INSTALL_RPATH=$INSTALLDIR/lib -DUSE_AVX_INSTRUCTIONS=1 && make install )
 ( cd server && cmake -DCMAKE_PREFIX_PATH=$INSTALLDIR -DOZ_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DCMAKE_INSTALL_RPATH=$INSTALLDIR/lib . && make install )
