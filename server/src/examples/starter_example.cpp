a1ac82d62c2b0ebc72a4acbf7b3a1a30458cff52
Pliable Pixels <pliablepixels@gmail.com>
2016-08-07 19:41:35 -0400

    new modules for API docs

--- server/src/examples/starter_example.cpp
+++ server/src/examples/starter_example.cpp
@@ -29,6 +29,7 @@ int main( int argc, const char *argv[] )
 
    	// Two RTSP sources 
     NetworkAVInput cam1( "cam1", "rtsp://170.93.143.139:1935/rtplive/0b01b57900060075004d823633235daa" );
+    //NetworkAVInput cam2( "cam2", "rtsp://170.93.143.139:1935/rtplive/0b01b57900060075004d823633235daa" );
     NetworkAVInput cam2("cam2","rtsp://170.93.143.139:1935/rtplive/e0ffa81e00a200ab0050fa36c4235c0a");
     app.addThread( &cam1 );
     app.addThread( &cam2 );
@@ -38,19 +39,10 @@ int main( int argc, const char *argv[] )
   	motionDetector1.registerProvider( cam1 );
    	app.addThread( &motionDetector1 );
 
-	// motion detect for cam1
+	// motion detect for cam2
 	MotionDetector motionDetector2( "modectcam2" );
   	motionDetector2.registerProvider( cam2 );
    	app.addThread( &motionDetector2 );
-
-	// Let's make a mux/stitched handler for cam1 and cam2 and its debugs
-	MatrixVideo matrixVideo( "matrixcammux", PIX_FMT_YUV420P, 640, 480, FrameRate( 1, 10 ), 2, 2 );
-   	matrixVideo.registerProvider( cam1 );
-   	matrixVideo.registerProvider( *motionDetector1.deltaImageSlave() );
-   	matrixVideo.registerProvider( cam2 );
-   	matrixVideo.registerProvider( *motionDetector2.deltaImageSlave() );
-   	app.addThread( &matrixVideo );
-
 
 	Listener listener;
     app.addThread( &listener );
@@ -59,11 +51,8 @@ int main( int argc, const char *argv[] )
     httpController.addStream("watchcam1",cam1);
     httpController.addStream("watchcam2",cam2);
 
-	httpController.addStream( "file", cam1 );
-   	httpController.addStream( "debug", SlaveVideo::cClass() );
-   	httpController.addStream( "debug", matrixVideo );
    	httpController.addStream( "debug", motionDetector1 );
-   	httpController.addStream( "debug", motionDetector2 );
+   httpController.addStream( "debug", motionDetector2 );
 	
     listener.addController( &httpController );
 
