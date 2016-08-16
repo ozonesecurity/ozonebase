aa2c20e0b176eb97b939d193a88d2df73bcc7768
Pliable Pixels <pliablepixels@gmail.com>
2016-08-16 13:14:54 -0500

    supress external image warning

--- docs/server/guide/conf.py
+++ docs/server/guide/conf.py
@@ -69,6 +69,7 @@ release = '0.1'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build']
+suppress_warnings = ['image.nonlocal_uri']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
