164e15fd7d797c2177d2d06e87051f8f3a4a1084
Pliable Pixels <pliablepixels@gmail.com>
2017-05-05 05:59:24 -0400

    removed permissive flag

--- server/src/libgen/libgenUtils.h
+++ server/src/libgen/libgenUtils.h
@@ -71,7 +71,7 @@ bool getBoolEnv( const char *varName, const char *defaultValue=0 );
 class StringTokenList
 {
 protected:
-    typedef char *(*StrFn)( char *, const char * );
+    typedef const char *(*StrFn)( char *, const char * );
 
 public:
     typedef std::deque<std::string> TokenList;
